a461d97c0a4f82714369a2b657fba627d3013e56
J Tang <jimmytang@gmail.com>
2012-06-27 11:46:05 -0700

    - Add tests for YozioDataStoreImpl

---
+++ yozio_android_x_test/src/com/yozio/android/YozioDataStoreImplTest.java
@@ -0,0 +1,155 @@
+package com.yozio.android;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+import com.yozio.android.YozioDataStore.Events;
+import com.yozio.android.YozioDataStoreImpl.DatabaseHelper;
+
+public class YozioDataStoreImplTest extends AndroidTestCase {
+  
+  private static final String LOGTAG = "YozioDataStoreImplTest";
+  private static final String APP_KEY = "test app key";
+  private static final String EVENT_KEY = "event key";
+  
+  private YozioDataStoreImpl dataStore;
+  
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    getContext().deleteDatabase(YozioDataStoreImpl.DATABASE_NAME);
+    dataStore = new YozioDataStoreImpl(new DatabaseHelper(getContext()), APP_KEY);
+  }
+  
+  public void testReadNoEvents() {
+    assertEquals(0, dataStore.getNumEvents());
+    assertNull(dataStore.getEvents(10));
+  }
+  
+  public void testAddEvent() {
+    JSONObject event1 = buildTestEvent("event 1");
+    JSONObject event2 = buildTestEvent("event 2");
+    JSONObject event3 = buildTestEvent("event 3");
+    
+    dataStore.addEvent(event1);
+    assertEquals(1, dataStore.getNumEvents());
+    assertEventsEqual(dataStore.getEvents(10).getJsonArray(), event1);
+    
+    dataStore.addEvent(event2);
+    assertEquals(2, dataStore.getNumEvents());
+    assertEventsEqual(dataStore.getEvents(10).getJsonArray(), event1, event2);
+    
+    dataStore.addEvent(event3);
+    assertEquals(3, dataStore.getNumEvents());
+    assertEventsEqual(dataStore.getEvents(10).getJsonArray(), event1, event2, event3);
+  }
+  
+  public void testGetEventWithLimit() {
+    JSONObject event1 = buildTestEvent("event 1");
+    JSONObject event2 = buildTestEvent("event 2");
+    JSONObject event3 = buildTestEvent("event 3");
+    dataStore.addEvent(event1);
+    dataStore.addEvent(event2);
+    dataStore.addEvent(event3);
+    assertNull(dataStore.getEvents(0));
+    assertEventsEqual(dataStore.getEvents(1).getJsonArray(), event1);
+    assertEventsEqual(dataStore.getEvents(2).getJsonArray(), event1, event2);
+    assertEventsEqual(dataStore.getEvents(3).getJsonArray(), event1, event2, event3);
+    assertEventsEqual(dataStore.getEvents(4).getJsonArray(), event1, event2, event3);
+  }
+  
+  public void testRemoveEvents() {
+    JSONObject event1 = buildTestEvent("event 1");
+    JSONObject event2 = buildTestEvent("event 2");
+    JSONObject event3 = buildTestEvent("event 3");
+    dataStore.addEvent(event1);
+    dataStore.addEvent(event2);
+    dataStore.addEvent(event3);
+    assertEquals(3, dataStore.getNumEvents());
+    Events events = dataStore.getEvents(10);
+    dataStore.removeEvents(events.getLastEventId());
+    assertEquals(0, dataStore.getNumEvents());
+  }
+  
+  public void testRemoveEventsWithLimit() {
+    JSONObject event1 = buildTestEvent("event 1");
+    JSONObject event2 = buildTestEvent("event 2");
+    JSONObject event3 = buildTestEvent("event 3");
+    dataStore.addEvent(event1);
+    dataStore.addEvent(event2);
+    dataStore.addEvent(event3);
+    assertEquals(3, dataStore.getNumEvents());
+    Events events = dataStore.getEvents(2);
+    dataStore.removeEvents(events.getLastEventId());
+    assertEquals(1, dataStore.getNumEvents());
+    assertEventsEqual(dataStore.getEvents(10).getJsonArray(), event3);
+  }
+  
+  public void testRemoveEventsWithNoEvents() {
+    assertEquals(0, dataStore.getNumEvents());
+    dataStore.removeEvents("10");
+    assertEquals(0, dataStore.getNumEvents());
+  }
+  
+  public void testAddAndRemoveEvents() {
+    JSONObject event1 = buildTestEvent("event 1");
+    JSONObject event2 = buildTestEvent("event 2");
+    JSONObject event3 = buildTestEvent("event 3");
+    JSONObject event4 = buildTestEvent("event 4");
+    JSONObject event5 = buildTestEvent("event 5");
+    dataStore.addEvent(event1);
+    dataStore.addEvent(event2);
+    dataStore.addEvent(event3);
+    assertEquals(3, dataStore.getNumEvents());
+    
+    Events events = dataStore.getEvents(1);
+    dataStore.removeEvents(events.getLastEventId());
+    assertEquals(2, dataStore.getNumEvents());
+    assertEventsEqual(dataStore.getEvents(10).getJsonArray(), event2, event3);
+    
+    dataStore.addEvent(event4);
+    events = dataStore.getEvents(1);
+    dataStore.removeEvents(events.getLastEventId());
+    assertEquals(2, dataStore.getNumEvents());
+    assertEventsEqual(dataStore.getEvents(10).getJsonArray(), event3, event4);
+    
+    dataStore.addEvent(event5);
+    events = dataStore.getEvents(2);
+    dataStore.removeEvents(events.getLastEventId());
+    assertEquals(1, dataStore.getNumEvents());
+    assertEventsEqual(dataStore.getEvents(10).getJsonArray(), event5);
+  }
+  
+  // Builds a JSONObject to represent an event.
+  // Does not have the same structure as an actual event.
+  private JSONObject buildTestEvent(String name) {
+    try {
+      JSONObject eventObject = new JSONObject();
+      eventObject.put(EVENT_KEY, name);
+      return eventObject;
+    } catch (JSONException e) {
+      Log.e(LOGTAG, "buildTestEvent", e);
+      fail("Faiure building test event");
+      return null;
+    }
+  }
+  
+  // Asserts that the JSONArray is equivalent to eventsArr.
+  private void assertEventsEqual(JSONArray eventsJsonArr, JSONObject... expectedEventsArr) {
+    assertEquals(eventsJsonArr.length(), expectedEventsArr.length);
+    for (int i = 0; i < expectedEventsArr.length; i++) {
+      try {
+        JSONObject event = (JSONObject) eventsJsonArr.get(i);
+        JSONObject expectedEvent = expectedEventsArr[i];
+        assertEquals(event.toString(), expectedEvent.toString());
+      } catch (JSONException e) {
+        Log.e(LOGTAG, "assertEventsEqual", e);
+        fail("Faiure comparing JSON objects");
+      }
+    }
+  }
+}
