739fe7703c9f6c77d18b084dcac5cf2afce46d9e
Dounan Shi <dounan@gmail.com>
2012-09-13 16:29:09 -0700

    - Updated EndToEndTest

--- yozio_test/src/com/yozio/android/EndToEndTest.java
+++ yozio_test/src/com/yozio/android/EndToEndTest.java
@@ -14,6 +14,8 @@ public class EndToEndTest extends AndroidTestCase {
   private static final String APP_KEY = "139b0e10-ba56-012f-e1d9-2837371df2a8";
   private static final String LINK_NAME = "testlink";
 
+  private YozioDataStore dataStore;
+
   @Override
   protected void setUp() throws Exception {
     super.setUp();
@@ -22,27 +24,60 @@ public class EndToEndTest extends AndroidTestCase {
     YozioApiServiceImpl apiService = new YozioApiServiceImpl(httpClient);
     apiService.setBaseUrl(TEST_BASE_URL);
     SQLiteOpenHelper dbHelper = new YozioDataStoreImpl.DatabaseHelper(getContext());
-    YozioDataStore dataStore = new YozioDataStoreImpl(dbHelper, APP_KEY);
+    dataStore = new YozioDataStoreImpl(dbHelper, APP_KEY);
     YozioHelper helper = new YozioHelper(dataStore, apiService);
     Yozio.setHelper(helper);
   }
 
   public void testEndToEnd() {
+    // Test sending OPEN_APP event
     Yozio.configure(getContext(), APP_KEY, "test secret key");
+
+    // Test blocking getUrl
     String url = Yozio.getUrl(LINK_NAME, "www.google.com");
+    Log.i("YozioTest", "getUrl(): " + url);
+
+    // Test non-blocking getUrl
     Yozio.getUrlAsync(LINK_NAME, "www.yahoo.com", new GetUrlCallback() {
       public void handleResponse(String url) {
         Log.i("YozioTest", "getUrlAsync(): " + url);
       }
     });
+
+    // Test sending LOGIN event
     Yozio.userLoggedIn("spaceman");
-    Log.i("YozioTest", "getUrl(): " + url);
+
+    // Test sending valid viewedLink event
     Yozio.viewedLink(LINK_NAME);
+
+    // Test sending valid sharedLink event
     Yozio.sharedLink(LINK_NAME);
+
     try {
       Thread.sleep(2000);
     } catch (InterruptedException e) {
       Log.e("YozioTest", "testEndToEnd", e);
+    }
+
+    Log.i("YozioTest", "number of events: " + dataStore.getNumEvents());
+
+    // Test sending user invalid viewedLink event to make sure it gets removed
+    // from the queue
+    Log.i("YozioTest", "# events before invalid link name: " + dataStore.getNumEvents());
+    Yozio.viewedLink("nonexistentlinkname");
+    Log.i("YozioTest", "# events right after invalid link name: " + dataStore.getNumEvents());
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      Log.e("YozioTest", "testEndToEnd", e);
+    }
+    Log.i("YozioTest", "# events after invalid link name: " + dataStore.getNumEvents());
+
+    Yozio.userLoggedIn("grrrrrrrrrreat");
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      Log.e("YozioTest", "testEndToEnd2", e);
     }
   }
 }
